734f72029a55fd37bf2d53687b83762d33b417a6
fmallqui <franco.emp@gmail.com>
2013-11-15 17:00:11 -0500

    Primer commit

---
+++ sistemaSBLM/src/com/sblm/bean/Columna.java
@@ -0,0 +1,196 @@
+package com.sblm.bean;
+
+public class Columna {
+	private String rep_ordentotal;
+	private String rep_ordenxdistrito;
+	private String rep_clave;
+	private String rep_direccion;
+	private String rep_numero;
+	private String rep_mazlote;
+	private String rep_stand;
+	private String rep_uso;
+	private String rep_distrito;
+
+	private String importeupa01;
+	private String importeupa02;
+	private String importeupa03;
+	private String importeupa04;
+	private String importeupa05;
+	private String importeupa06;
+	private String importeupa07;
+	private String importeupa08;
+	private String importeupa09;
+	private String importeupa10;
+	private String importeupa11;
+	private String importeupa12;
+	private String importeupa13;
+	private String importeupa14;
+	private String importeupa15;
+	private String importeupatotal;
+	private String rep_cbeneficio;
+	private String rep_observacion;
+	public String getRep_ordentotal() {
+		return rep_ordentotal;
+	}
+	public void setRep_ordentotal(String rep_ordentotal) {
+		this.rep_ordentotal = rep_ordentotal;
+	}
+	public String getRep_ordenxdistrito() {
+		return rep_ordenxdistrito;
+	}
+	public void setRep_ordenxdistrito(String rep_ordenxdistrito) {
+		this.rep_ordenxdistrito = rep_ordenxdistrito;
+	}
+	public String getRep_clave() {
+		return rep_clave;
+	}
+	public void setRep_clave(String rep_clave) {
+		this.rep_clave = rep_clave;
+	}
+	public String getRep_direccion() {
+		return rep_direccion;
+	}
+	public void setRep_direccion(String rep_direccion) {
+		this.rep_direccion = rep_direccion;
+	}
+	public String getRep_numero() {
+		return rep_numero;
+	}
+	public void setRep_numero(String rep_numero) {
+		this.rep_numero = rep_numero;
+	}
+	public String getRep_mazlote() {
+		return rep_mazlote;
+	}
+	public void setRep_mazlote(String rep_mazlote) {
+		this.rep_mazlote = rep_mazlote;
+	}
+	public String getRep_stand() {
+		return rep_stand;
+	}
+	public void setRep_stand(String rep_stand) {
+		this.rep_stand = rep_stand;
+	}
+	public String getRep_uso() {
+		return rep_uso;
+	}
+	public void setRep_uso(String rep_uso) {
+		this.rep_uso = rep_uso;
+	}
+	public String getRep_distrito() {
+		return rep_distrito;
+	}
+	public void setRep_distrito(String rep_distrito) {
+		this.rep_distrito = rep_distrito;
+	}
+	public String getImporteupa01() {
+		return importeupa01;
+	}
+	public void setImporteupa01(String importeupa01) {
+		this.importeupa01 = importeupa01;
+	}
+	public String getImporteupa02() {
+		return importeupa02;
+	}
+	public void setImporteupa02(String importeupa02) {
+		this.importeupa02 = importeupa02;
+	}
+	public String getImporteupa03() {
+		return importeupa03;
+	}
+	public void setImporteupa03(String importeupa03) {
+		this.importeupa03 = importeupa03;
+	}
+	public String getImporteupa04() {
+		return importeupa04;
+	}
+	public void setImporteupa04(String importeupa04) {
+		this.importeupa04 = importeupa04;
+	}
+	public String getImporteupa05() {
+		return importeupa05;
+	}
+	public void setImporteupa05(String importeupa05) {
+		this.importeupa05 = importeupa05;
+	}
+	public String getImporteupa06() {
+		return importeupa06;
+	}
+	public void setImporteupa06(String importeupa06) {
+		this.importeupa06 = importeupa06;
+	}
+	public String getImporteupa07() {
+		return importeupa07;
+	}
+	public void setImporteupa07(String importeupa07) {
+		this.importeupa07 = importeupa07;
+	}
+	public String getImporteupa08() {
+		return importeupa08;
+	}
+	public void setImporteupa08(String importeupa08) {
+		this.importeupa08 = importeupa08;
+	}
+	public String getImporteupa09() {
+		return importeupa09;
+	}
+	public void setImporteupa09(String importeupa09) {
+		this.importeupa09 = importeupa09;
+	}
+	public String getImporteupa10() {
+		return importeupa10;
+	}
+	public void setImporteupa10(String importeupa10) {
+		this.importeupa10 = importeupa10;
+	}
+	public String getImporteupa11() {
+		return importeupa11;
+	}
+	public void setImporteupa11(String importeupa11) {
+		this.importeupa11 = importeupa11;
+	}
+	public String getImporteupa12() {
+		return importeupa12;
+	}
+	public void setImporteupa12(String importeupa12) {
+		this.importeupa12 = importeupa12;
+	}
+	public String getImporteupa13() {
+		return importeupa13;
+	}
+	public void setImporteupa13(String importeupa13) {
+		this.importeupa13 = importeupa13;
+	}
+	public String getImporteupa14() {
+		return importeupa14;
+	}
+	public void setImporteupa14(String importeupa14) {
+		this.importeupa14 = importeupa14;
+	}
+	public String getImporteupa15() {
+		return importeupa15;
+	}
+	public void setImporteupa15(String importeupa15) {
+		this.importeupa15 = importeupa15;
+	}
+	public String getImporteupatotal() {
+		return importeupatotal;
+	}
+	public void setImporteupatotal(String importeupatotal) {
+		this.importeupatotal = importeupatotal;
+	}
+	public String getRep_cbeneficio() {
+		return rep_cbeneficio;
+	}
+	public void setRep_cbeneficio(String rep_cbeneficio) {
+		this.rep_cbeneficio = rep_cbeneficio;
+	}
+	public String getRep_observacion() {
+		return rep_observacion;
+	}
+	public void setRep_observacion(String rep_observacion) {
+		this.rep_observacion = rep_observacion;
+	}
+	
+
+}
---
+++ sistemaSBLM/src/com/sblm/service/IPaginaService.java
@@ -0,0 +1,37 @@
+package com.sblm.service;
+
+import java.util.Date;
+import java.util.List;
+
+import com.sblm.model.Pagina;
+import com.sblm.model.Paginamodulo;
+
+public interface IPaginaService {
+
+	public void registrarPagina(Pagina pagina);
+
+	public void actualizarPagina(Pagina pagina);
+
+	public void eliminarPagina(Pagina pagina);
+
+	public Pagina listarPaginaPorId(int id);
+
+	public List<Pagina> listarPaginas();
+	public List<Pagina> listarPaginasModulos(int idmodulo);
+	
+	public int obtenerNumeroPaginas();
+	public String obtenerUltimaPagina();
+	public Date obtenerFechaUltimaPagina();
+	 
+	public void registrarPaginamodulo(Paginamodulo paginamodulo);
+	public Pagina obtenerUltimaPaginaCreada();
+	public List<Paginamodulo> listarPaginamodulos();
+
+	void actualizarPaginamodulo(Paginamodulo paginamodulo);
+
+	void eliminarPaginamodulo(Paginamodulo paginamodulo);
+
+	Pagina verificarPaginaEnModulo(String descripcionpagina,String nombrepagina, int idmodulo);
+	
+	
+}
